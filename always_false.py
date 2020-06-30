63de4faec9de2ee031e55a150d10b4c4a06c2a3f
Christopher Gaughan <58716009+christophergaughan@users.noreply.github.com>
2020-06-30 16:07:22 -0400

    Add files via upload

---
+++ always_false.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+Create a function named always_false() that has one parameter named num.
+
+Using an if statement, your variable num, and the operators >, and <, make it so your function will return False no matter what number is stored in num.
+
+An if statement that is always false is called a contradiction. You will rarely want to do this while programming, but it is important to realize it is possible to do this.
+"""
+def always_false(num):
+	if 10 > num < 20:
+		return False
+	else:
+		return False
+		
+	
